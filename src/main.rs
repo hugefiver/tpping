d4ac9b3e78a0cded94f29b64f4f6a3d6f0c7283b
hugefiver <i@iruri.moe>
2020-09-22 20:53:03 +0800

    fix a typo

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ use tui::backend::CrosstermBackend;
 fn main() {
     let stdout = io::stdout();
     let backend = CrosstermBackend::new(stdout);
-    let mut term = Terminal::new(backend).expect("Can not start terminal UI. ");    
+    let mut term = Terminal::new(backend).expect("Can not start terminal UI. ");
 }
